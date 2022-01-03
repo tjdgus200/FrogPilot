6832676d66109ab51fe1419929220cb060d913a2
geniuth <74520084+geniuth@users.noreply.github.com>
2022-01-03 14:31:25 +0900

    Update values.py
    Change value function from opgm

--- selfdrive/car/gm/values.py
+++ selfdrive/car/gm/values.py
@@ -5,42 +5,95 @@ from selfdrive.car import dbc_dict
 Ecu = car.CarParams.Ecu
 
 class CarControllerParams():
-  STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
-  STEER_STEP = 2  # control frames per command
-  STEER_DELTA_UP = 7
-  STEER_DELTA_DOWN = 17
-  MIN_STEER_SPEED = 3.  # m/s
-  STEER_DRIVER_ALLOWANCE = 50
-  STEER_DRIVER_MULTIPLIER = 4
-  STEER_DRIVER_FACTOR = 100
-  NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
+    def __init__(self, CP):  
+    self.STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
+    self.STEER_STEP = 2  # control frames per command
+    self.STEER_DELTA_UP = 7
+    self.STEER_DELTA_DOWN = 17
+    self.MIN_STEER_SPEED = 3.  # m/s
+    self.STEER_DRIVER_ALLOWANCE = 50
+    self.STEER_DRIVER_MULTIPLIER = 4
+    self.STEER_DRIVER_FACTOR = 100
+    self.NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
+
+    # Heartbeat for dash "Service Adaptive Cruise" and "Service Front Camera"
+    self.ADAS_KEEPALIVE_STEP = 100
+    self.CAMERA_KEEPALIVE_STEP = 100
+
+    # Volt gasbrake lookups
+    self.MAX_GAS = 3072 # Safety limit, not ACC max. Stock ACC >4096 from standstill.
+    self.ZERO_GAS = 2048 # Coasting
+    self.MAX_BRAKE = 350 # ~ -3.5 m/s^2 with regen
+
+    # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
+    # perform the closed loop control, and might need some
+    # to apply some more braking if we're on a downhill slope.
+    # Our controller should still keep the 2 second average above
+    # -3.5 m/s^2 as per planner limits
+
+    # TODO if this bump works, it belongs in interface per car
+    self.ACCEL_MAX = 2. # m/s^2
+    self.ACCEL_MIN = -4. # m/s^2
+    
+    if CP.carFingerprint in NO_ASCM:
+      pass
+      #self.STEER_DELTA_UP = 3          # ~0.75s time to peak torque (255/50hz/0.75s)
+      #self.STEER_DELTA_DOWN = 7       # ~0.3s from peak torque to zero
+    elif CP.carFingerprint in HIGH_TORQUE:
+      self.STEER_MAX = 600  # Safety limit, not LKA max. Trucks use 600.
+      self.STEER_DELTA_UP = 14
+      self.STEER_DELTA_DOWN = 34
+      self.STEER_DRIVER_ALLOWANCE = 100
+      self.STEER_DRIVER_MULTIPLIER = 4
+      self.STEER_DRIVER_FACTOR = 100
+    
+    #if CP.enableGasInterceptor:
+    #TODO: Pedal may need different accel_max and min?  
+
+    self.MAX_ACC_REGEN = 1404  # Max ACC regen is slightly less than max paddle regen
+    self.GAS_LOOKUP_BP = [-1., 0., self.ACCEL_MAX]
+    self.GAS_LOOKUP_V = [self.MAX_ACC_REGEN, self.ZERO_GAS, self.MAX_GAS]
+    self.BRAKE_LOOKUP_BP = [self.ACCEL_MIN, -1.]
+    self.BRAKE_LOOKUP_V = [self.MAX_BRAKE, 0.]
+
+
+#STEER_THRESHOLD = 1.0
+
+
+#  STEER_MAX = 300  # Safety limit, not LKA max. Trucks use 600.
+#  STEER_STEP = 2  # control frames per command
+#  STEER_DELTA_UP = 7
+#  STEER_DELTA_DOWN = 17
+#  MIN_STEER_SPEED = 3.  # m/s
+#  STEER_DRIVER_ALLOWANCE = 50
+#  STEER_DRIVER_MULTIPLIER = 4
+#  STEER_DRIVER_FACTOR = 100
+#  NEAR_STOP_BRAKE_PHASE = 0.5  # m/s
 
   # Heartbeat for dash "Service Adaptive Cruise" and "Service Front Camera"
-  ADAS_KEEPALIVE_STEP = 100
-  CAMERA_KEEPALIVE_STEP = 100
+#  ADAS_KEEPALIVE_STEP = 100
+#  CAMERA_KEEPALIVE_STEP = 100
 
   # pedal lookups, only for Volt
-  MAX_GAS = 3072  # Only a safety limit
-  ZERO_GAS = 2048
-  MAX_BRAKE = 350  # Should be around 3.5m/s^2, including regen
+#  MAX_GAS = 3072  # Only a safety limit
+#  ZERO_GAS = 2048
+#  MAX_BRAKE = 350  # Should be around 3.5m/s^2, including regen
 
   # Allow small margin below -3.5 m/s^2 from ISO 15622:2018 since we
   # perform the closed loop control, and might need some
 
   # Our controller should still keep the 2 second average above
   # -3.5 m/s^2 as per planner limits
-  ACCEL_MAX = 2.0
-  ACCEL_MIN = -4.0
-  ACCEL_SCALE = 4.0  # max(ACCEL_MAX, -ACCEL_MIN)
+#  ACCEL_MAX = 2.0
+#  ACCEL_MIN = -4.0
+#  ACCEL_SCALE = 4.0  # max(ACCEL_MAX, -ACCEL_MIN)
 
-  MAX_ACC_REGEN = 1404  # ACC Regen braking is slightly less powerful than max regen paddle
-  GAS_LOOKUP_BP = [-1, 0., ACCEL_MAX]
-  GAS_LOOKUP_V = [MAX_ACC_REGEN, ZERO_GAS, MAX_GAS]
-  BRAKE_LOOKUP_BP = [ACCEL_MIN, -0.1]
-  BRAKE_LOOKUP_V = [MAX_BRAKE, 0]
-  ACCEL_HYST_GAP = 0.02
-
-STEER_THRESHOLD = 1.0
+#  MAX_ACC_REGEN = 1404  # ACC Regen braking is slightly less powerful than max regen paddle
+#  GAS_LOOKUP_BP = [-1, 0., ACCEL_MAX]
+#  GAS_LOOKUP_V = [MAX_ACC_REGEN, ZERO_GAS, MAX_GAS]
+#  BRAKE_LOOKUP_BP = [ACCEL_MIN, -0.1]
+#  BRAKE_LOOKUP_V = [MAX_BRAKE, 0]
+#  ACCEL_HYST_GAP = 0.02
 
 class CAR:
   HOLDEN_ASTRA = "HOLDEN ASTRA RS-V BK 2017"
