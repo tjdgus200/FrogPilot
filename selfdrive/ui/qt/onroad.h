5787042ed4eb7eada55f72b0756c52d7fec61575
jc01rho <rkjnice@gmail.com>
2022-01-06 15:04:24 +0900

    reverted ui changes

--- selfdrive/ui/qt/onroad.h
+++ selfdrive/ui/qt/onroad.h
@@ -7,11 +7,6 @@
 #include "selfdrive/ui/ui.h"
 
 
-#ifdef QCOM2
-#include <QTimer>
-#include "selfdrive/ui/qt/screenrecorder/screenrecorder.h"
-#endif
-
 // ***** onroad widgets *****
 
 class OnroadHud : public QWidget {
@@ -27,27 +22,24 @@ class OnroadHud : public QWidget {
   Q_PROPERTY(bool mainOn MEMBER mainOn NOTIFY valueChanged);
   Q_PROPERTY(bool lkasEnabled MEMBER lkasEnabled NOTIFY valueChanged);
   Q_PROPERTY(bool adaptiveCruise MEMBER adaptiveCruise NOTIFY valueChanged);
-  Q_PROPERTY(bool show_debug MEMBER show_debug NOTIFY valueChanged);
 
 
 
 public:
-    explicit OnroadHud(QWidget *parent);
-    void updateState(const UIState &s);
+  explicit OnroadHud(QWidget *parent);
+  void updateState(const UIState &s);
 
 private:
   void drawIcon(QPainter &p, int x, int y, QPixmap &img, QBrush bg, float opacity);
   void drawText(QPainter &p, int x, int y, const QString &text, int alpha = 255);
   void paintEvent(QPaintEvent *event) override;
-    void drawText2(QPainter &p, int x, int y, int flags, const QString &text, const QColor& color);
-    void drawTextWithColor(QPainter &p, int x, int y, const QString &text, QColor& color);
 
   QPixmap engage_img;
   QPixmap dm_img;
   QPixmap brake_img;
   QPixmap lat_icon_img;
   QPixmap long_img;
-  const int radius = 160;
+  const int radius = 192;
   const int img_size = (radius / 2) * 1.5;
   QString speed;
   QString speedUnit;
@@ -60,75 +52,63 @@ private:
   bool mainOn = false;
   bool lkasEnabled = false;
   bool adaptiveCruise = false;
-  bool show_debug = false;
-
-
-    void drawMaxSpeed(QPainter &p);
-    void drawSpeed(QPainter &p);
-    void drawBottomIcons(QPainter &p);
-    void drawSpeedLimit(QPainter &p);
-    void drawTurnSignals(QPainter &p);
-    void drawGpsStatus(QPainter &p);
-    void drawDebugText(QPainter &p);
-    void drawHud(QPainter &p);
-
 
 signals:
   void valueChanged();
 };
 
 class OnroadAlerts : public QWidget {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    OnroadAlerts(QWidget *parent = 0) : QWidget(parent) {};
-    void updateAlert(const Alert &a, const QColor &color);
+  OnroadAlerts(QWidget *parent = 0) : QWidget(parent) {};
+  void updateAlert(const Alert &a, const QColor &color);
 
 protected:
-    void paintEvent(QPaintEvent*) override;
+  void paintEvent(QPaintEvent*) override;
 
 private:
-    QColor bg;
-    Alert alert = {};
+  QColor bg;
+  Alert alert = {};
 };
 
 // container window for the NVG UI
 class NvgWindow : public CameraViewWidget {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit NvgWindow(VisionStreamType type, QWidget* parent = 0) : CameraViewWidget("camerad", type, true, parent) {}
+  explicit NvgWindow(VisionStreamType type, QWidget* parent = 0) : CameraViewWidget("camerad", type, true, parent) {}
 
 protected:
-    void paintGL() override;
-    void initializeGL() override;
-    void showEvent(QShowEvent *event) override;
-    void updateFrameMat(int w, int h) override;
-    void drawLaneLines(QPainter &painter, const UIScene &scene);
-    void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
-    inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
-    double prev_draw_t = 0;
+  void paintGL() override;
+  void initializeGL() override;
+  void showEvent(QShowEvent *event) override;
+  void updateFrameMat(int w, int h) override;
+  void drawLaneLines(QPainter &painter, const UIScene &scene);
+  void drawLead(QPainter &painter, const cereal::ModelDataV2::LeadDataV3::Reader &lead_data, const QPointF &vd);
+  inline QColor redColor(int alpha = 255) { return QColor(201, 34, 49, alpha); }
+  double prev_draw_t = 0;
 };
 
 // container for all onroad widgets
 class OnroadWindow : public QWidget {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    explicit OnroadWindow(QWidget* parent = 0);
-    bool isMapVisible() const { return map && map->isVisible(); }
+  OnroadWindow(QWidget* parent = 0);
+  bool isMapVisible() const { return map && map->isVisible(); }
 
 private:
-    void paintEvent(QPaintEvent *event);
-    void mousePressEvent(QMouseEvent* e) override;
-    OnroadHud *hud;
-    OnroadAlerts *alerts;
-    NvgWindow *nvg;
-    QColor bg = bg_colors[STATUS_DISENGAGED];
-    QWidget *map = nullptr;
-    QHBoxLayout* split;
+  void paintEvent(QPaintEvent *event);
+  void mousePressEvent(QMouseEvent* e) override;
+  OnroadHud *hud;
+  OnroadAlerts *alerts;
+  NvgWindow *nvg;
+  QColor bg = bg_colors[STATUS_DISENGAGED];
+  QWidget *map = nullptr;
+  QHBoxLayout* split;
 
 private slots:
-            void offroadTransition(bool offroad);
-    void updateState(const UIState &s);
+  void offroadTransition(bool offroad);
+  void updateState(const UIState &s);
 };
