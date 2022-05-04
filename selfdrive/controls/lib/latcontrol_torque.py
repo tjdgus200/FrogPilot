f449f047904ad1a5403928ff8b7b5955f4520163
whrho <whrho@sparrowfasoo.com>
2022-05-04 17:40:51 +0900

    fixed..

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -1,122 +1,109 @@
 import math
-import numpy as np
 
 from cereal import log
-from common.filter_simple import FirstOrderFilter
-from common.numpy_fast import clip, interp
-from common.realtime import DT_CTRL
+from common.numpy_fast import interp
 from selfdrive.controls.lib.latcontrol import LatControl, MIN_STEER_SPEED
+from selfdrive.controls.lib.pid import PIDController
+from selfdrive.controls.lib.vehicle_model import ACCELERATION_DUE_TO_GRAVITY
 from selfdrive.ntune import nTune
+from selfdrive.controls.lib.latcontrol_pid import ERROR_RATE_FRAME
+
+# At higher speeds (25+mph) we can assume:
+# Lateral acceleration achieved by a specific car correlates to
+# torque applied to the steering rack. It does not correlate to
+# wheel slip, or to speed.
+
+# This controller applies torque to achieve desired lateral
+# accelerations. To compensate for the low speed effects we
+# use a LOW_SPEED_FACTOR in the error. Additionally, there is
+# friction in the steering wheel that needs to be overcome to
+# move it at all, this is compensated for too.
 
 
-class LatControlINDI(LatControl):
+LOW_SPEED_FACTOR = 200
+JERK_THRESHOLD = 0.2
+
+def apply_deadzone(error, deadzone):
+  if error > deadzone:
+    error -= deadzone
+  elif error < - deadzone:
+    error += deadzone
+  else:
+    error = 0.
+  return error
+
+class LatControlTorque(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
-    self.angle_steers_des = 0.
-
-    A = np.array([[1.0, DT_CTRL, 0.0],
-                  [0.0, 1.0, DT_CTRL],
-                  [0.0, 0.0, 1.0]])
-    C = np.array([[1.0, 0.0, 0.0],
-                  [0.0, 1.0, 0.0]])
-
-    # Q = np.matrix([[1e-2, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 10.0]])
-    # R = np.matrix([[1e-2, 0.0], [0.0, 1e3]])
-
-    # (x, l, K) = control.dare(np.transpose(A), np.transpose(C), Q, R)
-    # K = np.transpose(K)
-    K = np.array([[7.30262179e-01, 2.07003658e-04],
-                  [7.29394177e+00, 1.39159419e-02],
-                  [1.71022442e+01, 3.38495381e-02]])
-
-    self.speed = 0.
-
-    self.K = K
-    self.A_K = A - np.dot(K, C)
-    self.x = np.array([[0.], [0.], [0.]])
-
-    self._RC = (CP.lateralTuning.indi.timeConstantBP, CP.lateralTuning.indi.timeConstantV)
-    self._G = (CP.lateralTuning.indi.actuatorEffectivenessBP, CP.lateralTuning.indi.actuatorEffectivenessV)
-    self._outer_loop_gain = (CP.lateralTuning.indi.outerLoopGainBP, CP.lateralTuning.indi.outerLoopGainV)
-    self._inner_loop_gain = (CP.lateralTuning.indi.innerLoopGainBP, CP.lateralTuning.indi.innerLoopGainV)
-
+    self.CP = CP
+    self.pid = PIDController(CP.lateralTuning.torque.kp, CP.lateralTuning.torque.ki,
+                             k_d=CP.lateralTuning.torque.kd,
+                             k_f=CP.lateralTuning.torque.kf, pos_limit=self.steer_max, neg_limit=-self.steer_max)
+    self.get_steer_feedforward = CI.get_steer_feedforward_function()
+    self.use_steering_angle = CP.lateralTuning.torque.useSteeringAngle
+    self.friction = CP.lateralTuning.torque.friction
+    self.deadzone = CP.lateralTuning.torque.deadzone
+    self.errors = []
     self.tune = nTune(CP, self)
-    self.steer_filter = FirstOrderFilter(0., self.RC, DT_CTRL)
-    self.reset()
-
-  @property
-  def RC(self):
-    return interp(self.speed, self._RC[0], self._RC[1])
-
-  @property
-  def G(self):
-    return interp(self.speed, self._G[0], self._G[1])
-
-  @property
-  def outer_loop_gain(self):
-    return interp(self.speed, self._outer_loop_gain[0], self._outer_loop_gain[1])
-
-  @property
-  def inner_loop_gain(self):
-    return interp(self.speed, self._inner_loop_gain[0], self._inner_loop_gain[1])
 
   def reset(self):
     super().reset()
-    self.steer_filter.x = 0.
-    self.speed = 0.
+    self.pid.reset()
+    self.errors = []
 
   def update(self, active, CS, VM, params, last_actuators, desired_curvature, desired_curvature_rate, llk):
     self.tune.check()
-    self.speed = CS.vEgo
-    # Update Kalman filter
-    y = np.array([[math.radians(CS.steeringAngleDeg)], [math.radians(CS.steeringRateDeg)]])
-    self.x = np.dot(self.A_K, self.x) + np.dot(self.K, y)
-
-    indi_log = log.ControlsState.LateralINDIState.new_message()
-    indi_log.steeringAngleDeg = math.degrees(self.x[0])
-    indi_log.steeringRateDeg = math.degrees(self.x[1])
-    indi_log.steeringAccelDeg = math.degrees(self.x[2])
-
-    steers_des = VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll)
-    steers_des += math.radians(params.angleOffsetDeg)
-    indi_log.steeringAngleDesiredDeg = math.degrees(steers_des)
-
-    rate_des = VM.get_steer_from_curvature(-desired_curvature_rate, CS.vEgo, 0)
-    indi_log.steeringRateDesiredDeg = math.degrees(rate_des)
+    pid_log = log.ControlsState.LateralTorqueState.new_message()
 
     if CS.vEgo < MIN_STEER_SPEED or not active:
-      indi_log.active = False
-      self.steer_filter.x = 0.0
-      output_steer = 0
+      output_torque = 0.0
+      pid_log.active = False
+      self.pid.reset()
+      angle_steers_des = 0.0
     else:
-      # Expected actuator value
-      self.steer_filter.update_alpha(self.RC)
-      self.steer_filter.update(last_actuators.steer)
+      if self.use_steering_angle:
+        actual_curvature = -VM.calc_curvature(math.radians(CS.steeringAngleDeg - params.angleOffsetDeg), CS.vEgo, params.roll)
+      else:
+        actual_curvature = llk.angularVelocityCalibrated.value[2] / CS.vEgo
+      desired_lateral_accel = desired_curvature * CS.vEgo ** 2
+      desired_lateral_jerk = desired_curvature_rate * CS.vEgo ** 2
+      actual_lateral_accel = actual_curvature * CS.vEgo ** 2
 
-      # Compute acceleration error
-      rate_sp = self.outer_loop_gain * (steers_des - self.x[0]) + rate_des
-      accel_sp = self.inner_loop_gain * (rate_sp - self.x[1])
-      accel_error = accel_sp - self.x[2]
+      setpoint = desired_lateral_accel + LOW_SPEED_FACTOR * desired_curvature
+      measurement = actual_lateral_accel + LOW_SPEED_FACTOR * actual_curvature
+      error = setpoint - measurement
 
-      # Compute change in actuator
-      g_inv = 1. / self.G
-      delta_u = g_inv * accel_error
+      error_rate = 0
+      if len(self.errors) >= ERROR_RATE_FRAME:
+        error_rate = (error - self.errors[-ERROR_RATE_FRAME]) / ERROR_RATE_FRAME
 
-      # If steering pressed, only allow wind down
-      if CS.steeringPressed and (delta_u * last_actuators.steer > 0):
-        delta_u = 0
+      self.errors.append(float(error))
+      while len(self.errors) > ERROR_RATE_FRAME:
+        self.errors.pop(0)
 
-      output_steer = self.steer_filter.x + delta_u
+      error_deadzone = apply_deadzone(error, self.deadzone)
 
-      output_steer = clip(output_steer, -self.steer_max, self.steer_max)
+      pid_log.error = error_deadzone
 
-      indi_log.active = True
-      indi_log.rateSetPoint = float(rate_sp)
-      indi_log.accelSetPoint = float(accel_sp)
-      indi_log.accelError = float(accel_error)
-      indi_log.delayedOutput = float(self.steer_filter.x)
-      indi_log.delta = float(delta_u)
-      indi_log.output = float(output_steer)
-      indi_log.saturated = self._check_saturation(self.steer_max - abs(output_steer) < 1e-3, CS)
+      ff = desired_lateral_accel - params.roll * ACCELERATION_DUE_TO_GRAVITY
+      # convert friction into lateral accel units for feedforward
+      friction_compensation = interp(desired_lateral_jerk, [-JERK_THRESHOLD, JERK_THRESHOLD],
+                                     [-self.friction, self.friction])
+      ff += friction_compensation / self.CP.lateralTuning.torque.kf
+      output_torque = self.pid.update(error_deadzone, error_rate,
+                                      override=CS.steeringPressed, feedforward=ff,
+                                      speed=CS.vEgo,
+                                      freeze_integrator=CS.steeringRateLimited)
 
-    return float(output_steer), float(steers_des), indi_log
+      pid_log.active = True
+      pid_log.p = self.pid.p
+      pid_log.i = self.pid.i
+      pid_log.d = self.pid.d
+      pid_log.f = self.pid.f
+      pid_log.output = -output_torque
+      pid_log.saturated = self._check_saturation(self.steer_max - abs(output_torque) < 1e-3, CS)
+
+      angle_steers_des = math.degrees(VM.get_steer_from_curvature(-desired_curvature, CS.vEgo, params.roll)) + params.angleOffsetDeg
+
+    # TODO left is positive in this convention
+    return -output_torque, angle_steers_des, pid_log
