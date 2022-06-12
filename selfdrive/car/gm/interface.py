813b44a0ff0356f6182db5599e9c01df0d7406f6
hazyavocado <71194240+hazyavocado@users.noreply.github.com>
2022-06-12 09:26:58 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -62,7 +62,7 @@ class CarInterface(CarInterfaceBase):
   def get_steer_feedforward_function(self):
     if self.CP.carFingerprint == CAR.BOLT_EUV:
       return self.get_steer_feedforward_bolt_euv
-    elif self.CP.carFingerprint == CAR.BOLT_NR:
+    elif self.CP.carFingerprint == CAR.BOLT_NR or self.CP.carFingerprint == CAR.BOLT:
       return self.get_steer_feedforward_bolt
     else:
       return CarInterfaceBase.get_steer_feedforward_default
