98e2156b640f8a051a74963418f14d805b0f79d8
geniuth <geniuth@gmail.com>
2023-12-27 15:09:47 +0900

    Revert "번호 수정"
    This reverts commit 1002396a775630a09a89fe427084bb67d0f012cd.

--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -48,8 +48,8 @@ def manager_init() -> None:
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
     ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
-    ("AutoNaviSpeedCtrlStart", "25"),
-    ("AutoNaviSpeedCtrlEnd", "15"),
+    ("AutoNaviSpeedCtrlStart", "22"),
+    ("AutoNaviSpeedCtrlEnd", "11"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
