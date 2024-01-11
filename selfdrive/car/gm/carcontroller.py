12da92ef454c799eced59c0cdbe416eb26fcecb0
geniuth <geniuth@gmail.com>
2024-01-11 14:30:04 +0900

    for BoltEV  - When calculating the pedal value,Change the formula to use the accel value as is without processing it and tune the longitudinal control

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -68,7 +68,7 @@ class CarController:
       pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.0, 0.190, 0.22, 0.280])
     else:
       pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.15, 0.190, 0.22, 0.280])
-    pedal_gas = clip((pedaloffset + accel * accGain), 0.0, 1.0)
+    pedal_gas = clip((pedaloffset + accel), 0.0, 1.0)
 
     return pedal_gas
 
