fb9710ff862549d2caca2afd896b885382c0239f
geniuth <geniuth@gmail.com>
2024-01-15 10:00:06 +0900

    Pedal longcontrol tuned

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -343,7 +343,7 @@ class CarInterface(CarInterfaceBase):
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_PEDAL_LONG
         # Note: Low speed, stop and go not tested. Should be fairly smooth on highway
         ret.longitudinalTuning.kpBP = [5., 35.]
-        ret.longitudinalTuning.kpV = [0.1833, 0.1667]
+        ret.longitudinalTuning.kpV = [0.1833, 0.1500]
         ret.longitudinalTuning.kiBP = [0., 35.0]
         ret.longitudinalTuning.kiV = [0.05, 0.05]
         ret.longitudinalTuning.kf = 0.15
