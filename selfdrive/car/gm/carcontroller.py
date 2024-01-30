5ed37af093f88c9c3001da592c2f460102dd0d71
geniuth <geniuth@gmail.com>
2024-01-30 23:06:37 +0900

    정지거리를 위해 롱컨 살짝 수정

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -67,7 +67,7 @@ class CarController:
     if accel > 0:
       pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.0, 0.180, 0.22, 0.280])
     else:
-      pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.08, 0.180, 0.22, 0.280])
+      pedaloffset = interp(car_velocity, [0., 3, 6, 30], [0.10, 0.180, 0.22, 0.280])
     pedal_gas = clip((pedaloffset + accel), 0.0, 1.0)
 
     return pedal_gas
