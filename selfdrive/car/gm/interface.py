13abc3a060fc162bb6b8938c16de9f90ae4bfb49
geniuth <geniuth@gmail.com>
2024-04-11 22:18:58 +0900

    boltpilot pedal 적용

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -265,10 +265,10 @@ class CarInterface(CarInterfaceBase):
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_PEDAL_LONG
         # Note: Low speed, stop and go not tested. Should be fairly smooth on highway
         ret.longitudinalTuning.kpBP = [5., 35.]
-        ret.longitudinalTuning.kpV = [0.35, 0.5]
+        ret.longitudinalTuning.kpV = [0.3250, 0.425]
         ret.longitudinalTuning.kiBP = [0., 35.0]
-        ret.longitudinalTuning.kiV = [0.1, 0.1]
-        ret.longitudinalTuning.kf = 0.15
+        ret.longitudinalTuning.kiV = [0.07, 0.1]
+        ret.longitudinalTuning.kf = 0.23
         ret.stoppingDecelRate = 0.8
       else:  # Pedal used for SNG, ACC for longitudinal control otherwise
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
