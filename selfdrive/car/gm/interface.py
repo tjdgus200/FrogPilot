a8bc11ab5d60be3ee40ab67c9946261b425405e5
geniuth2 <141710362+geniuth2@users.noreply.github.com>
2024-10-20 21:33:36 +0900

    Update interface.py
    페달 튜닝

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -297,14 +297,14 @@ class CarInterface(CarInterfaceBase):
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_PEDAL_LONG
         # Note: Low speed, stop and go not tested. Should be fairly smooth on highway
         if use_new_api:
-          ret.longitudinalTuning.kiBP = [0.0, 5., 35.]
-          ret.longitudinalTuning.kiV = [0.0, 0.35, 0.5]
+          ret.longitudinalTuning.kiBP = [0., 3., 6., 35.]
+          ret.longitudinalTuning.kiV = [0.10, 0.175, 0.225, 0.33]
         else:
-          ret.longitudinalTuning.kpBP = [5., 35.]
-          ret.longitudinalTuning.kpV = [0.35, 0.5]
+          ret.longitudinalTuning.kpBP = [0., 3., 6., 35.]
+          ret.longitudinalTuning.kpV = [0.10, 0.175, 0.225, 0.33]
           ret.longitudinalTuning.kiBP = [0., 35.0]
-          ret.longitudinalTuning.kiV = [0.1, 0.1]
-        ret.longitudinalTuning.kf = 0.15
+          ret.longitudinalTuning.kiV = [0.07, 0.07]
+        ret.longitudinalTuning.kf = 0.25
         ret.stoppingDecelRate = 0.8
       else:  # Pedal used for SNG, ACC for longitudinal control otherwise
         ret.safetyConfigs[0].safetyParam |= Panda.FLAG_GM_HW_CAM_LONG
